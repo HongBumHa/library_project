9bb1d1b2fb93ff9295960d6b80b577c288439f41
Bum <goldstate@naver.com>
2017-11-10 14:07:45 +0900

    패키지셋팅

---
+++ src/test/java/kr/or/dgit/libray_project/service/TestServiceUserRank.java
@@ -0,0 +1,14 @@
+package kr.or.dgit.libray_project.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestServiceUserRank {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
