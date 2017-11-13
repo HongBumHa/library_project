e913a77efed881ed49c67888c6fdb92732f20b99
JaeJin Bae <bjj7425@naver.com>
2017-11-13 15:46:51 +0900

    publisher 완료

--- src/main/java/kr/or/dgit/library_project/dao/PublisherDao.java
+++ src/main/java/kr/or/dgit/library_project/dao/PublisherDao.java
@@ -7,5 +7,7 @@ import kr.or.dgit.library_project.dto.Publisher;
 public interface PublisherDao {
 	List<Publisher> selectPublisherByAll();
 	Publisher selectPublisherByCodeName(Publisher publisher);
-	
+	int insertPublisher(Publisher publisher);
+	int updatePublisher(Publisher publisher);
+	int deletePublisher(Publisher publisher);
 }
---
+++ src/test/java/kr/or/dgit/library_project/service/TestServiceBook.java
@@ -0,0 +1,24 @@
+package kr.or.dgit.library_project.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestServiceBook {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
