e7fe74aea992e166f760991b6c11b1745e41e3be
JaeJin Bae <bjj7425@naver.com>
2017-11-28 16:38:37 +0900

    ui 레알최종수정 11/28 16:38

--- src/main/java/kr/or/dgit/library_project/ui/UserInfo.java
+++ src/main/java/kr/or/dgit/library_project/ui/UserInfo.java
@@ -27,7 +27,7 @@ public class UserInfo extends JFrame {
 		setTitle("DGIT LIBRARY USER MODE");
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 868, 579);
+		setBounds(100, 100, 833, 579);
 
 
 		contentPane = new JPanel();
