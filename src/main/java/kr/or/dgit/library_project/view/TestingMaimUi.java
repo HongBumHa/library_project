3bb18fdcb1907300d97e19e1a771400ebee55716
SeoJin Lee <imy123123@naver.com>
2017-11-28 11:36:37 +0900

    error

--- src/main/java/kr/or/dgit/library_project/view/TestingMaimUi.java
+++ src/main/java/kr/or/dgit/library_project/view/TestingMaimUi.java
@@ -32,16 +32,13 @@ public class TestingMaimUi {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 1037, 620);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		RentalBookPanel panel = new RentalBookPanel();
+		ManagerRentalBook panel = new ManagerRentalBook();
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 	
 		
